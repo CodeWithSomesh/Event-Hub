a31b62ab550b01de38673534fae561c4b0d44771
CodeWithSomesh <codewithsomesh@gmail.com>
2024-04-20 14:24:34 +0800

    Finishing up header component

---
+++ components/shared/Header.tsx
@@ -0,0 +1,29 @@
+import { SignedOut } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "../ui/button"
+
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36 flex text-nowrap items-center">
+          <Image alt="Event Hub Logo" width={55} height={48} src="/assets/images/logo7.png"/>
+          <h1 className="text-2xl font-extrabold">Event Hub</h1>
+        </Link>
+
+        <div className="flex w-32 justify-end gap-3">
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">
+                Login
+              </Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
