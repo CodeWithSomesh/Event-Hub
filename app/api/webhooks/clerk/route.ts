1a232c4f3200f3fe1d4deefbd517d79b7a0887e5
CodeWithSomesh <codewithsomesh@gmail.com>
2024-04-22 00:21:10 +0800

    Some typo made

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -86,7 +86,6 @@ export async function POST(req: Request) {
     const {id, image_url, first_name, last_name, username} = evt.data
 
     const user = {
-      clerkId: id, 
       username: username!,
       firstName: first_name!,
       lastName: last_name!,
