e1f853b8aea09e8eca1ede8bed90f20bab6bc40a
CodeWithSomesh <codewithsomesh@gmail.com>
2024-04-22 01:11:52 +0800

    More chnages

--- types/index.ts
+++ types/index.ts
@@ -1,66 +1,66 @@
 // ====== USER PARAMS ============
 export type  CreateUserParams = {
-    clerkId: string
-    firstName: string
-    lastName: string
-    username: string
-    email: string
-    photo: string
+    clerkId: string;
+    firstName: string;
+    lastName: string;
+    username: string;
+    email: string;
+    photo: string;
   }
   
 export type UpdateUserParams = {
-    firstName: string
-    lastName: string
-    username: string
-    photo: string
+    firstName: string;
+    lastName: string;
+    username: string;
+    photo: string;
 }
 
 // ====== EVENT PARAMS ======
 export type CreateEventParams = {
-    userId: string
+    userId: string;
     event: {
-      title: string
-      description: string
-      location: string
-      imageUrl: string
-      startDateTime: Date
-      endDateTime: Date
-      categoryId: string
-      price: string
-      isFree: boolean
-      url: string
+      title: string;
+      description: string;
+      location: string;
+      imageUrl: string;
+      startDateTime: Date;
+      endDateTime: Date;
+      categoryId: string;
+      price: string;
+      isFree: boolean;
+      url: string;
     }
-    path: string
+    path: string;
   }
   
   export type UpdateEventParams = {
     userId: string
     event: {
-      _id: string
-      title: string
-      imageUrl: string
-      description: string
-      location: string
-      startDateTime: Date
-      endDateTime: Date
-      categoryId: string
-      price: string
-      isFree: boolean
-      url: string
+      _id: string;
+      title: string;
+      imageUrl: string;
+      description: string;
+      location: string;
+      startDateTime: Date;
+      endDateTime: Date;
+      categoryId: string;
+      price: string;
+      isFree: boolean;
+      url: string;
     }
-    path: string
+    path: string;
   }
   
   export type DeleteEventParams = {
-    eventId: string
-    path: string
+    eventId: string;
+    path: string;
   }
   
   export type GetAllEventsParams = {
-    query: string
-    category: string
-    limit: number
-    page: number
+    query: string;
+    category: string;
+    limit: number;
+    page: number;
   }
   
   export type GetEventsByUserParams = {
