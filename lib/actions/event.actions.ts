c811e892fe0cecee5a39fe6e92a74fb31be9c657
CodeWithSomesh <codewithsomesh@gmail.com>
2024-05-03 11:01:25 +0800

    Added populate function in 'event.actions.ts'

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -6,6 +6,7 @@ import { connectToDatabase } from "../database";
 import Event from "../database/models/event.model";
 import User from "../database/models/user.model";
 import { revalidatePath } from 'next/cache'
+import Category from "../database/models/category.model";
 
 export const createEvent = async ({event, userId, path}: CreateEventParams) => {
 
@@ -24,6 +25,34 @@ export const createEvent = async ({event, userId, path}: CreateEventParams) => {
         revalidatePath(path)
 
         return JSON.parse(JSON.stringify(newEvent));
+    } catch (error) {
+        handleError(error)
+    }
+}
+
+
+
+const populateEvent = async(query:any) => {
+    return query
+        .populate({path: 'organizer', model: User, select: '_id firstName lastName'})
+        .populate({path: 'category', model: Category, select: '_id name'})
+}
+
+export const getEventById = async (eventId: string) => {
+
+    try{
+        //Connect to the Database
+        await connectToDatabase();
+
+        // Finding an event by its ID
+        const event = await populateEvent(Event.findById(eventId))
+
+        // If there is no such event then display this error
+        if (!event){
+            throw new Error('Event not found in Database')
+        }
+
+        return JSON.parse(JSON.stringify(event));
     } catch (error) {
         handleError(error)
     }
