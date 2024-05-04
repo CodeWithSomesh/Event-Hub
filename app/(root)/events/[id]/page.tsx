64382845ed05eee53d0d0870764ace18033986d0
CodeWithSomesh <codewithsomesh@gmail.com>
2024-05-04 19:53:09 +0800

    Added Event Card Interface and solved some HTML bugs

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -70,7 +70,7 @@ const EventDetails = async({params: {id}}: SearchParamProps) => {
           </div>
 
           <div className="flex flex-col gap-2">
-            <p className="p-bold-20 text-grey-600">What You'll Learn:</p>
+            <p className="p-bold-20 text-grey-600">What You'll Learn &#58;</p>
             <p className="p-medium-16 lg:p-regular-18">{event.description}</p>
             <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 underline">{event.url}</p>
           </div>
