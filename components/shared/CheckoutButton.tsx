7d3c07636d2672e33c47dd102dedf4488247530e
CodeWithSomesh <codewithsomesh@gmail.com>
2024-05-25 09:32:12 +0800

    Fixed some vercel bug

--- components/shared/CheckoutButton.tsx
+++ components/shared/CheckoutButton.tsx
@@ -5,7 +5,6 @@ import { SignedIn, SignedOut, useUser } from '@clerk/nextjs'
 import React from 'react'
 import { Button } from '../ui/button'
 import Link from 'next/link'
-import Checkout from './Checkout'
 import CheckoutModal from './CheckoutModal'
 
 const CheckoutButton = ({event} : {event : IEvent}) => {
